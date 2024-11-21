112192400679883e6c43eefa6892eefce8695a88
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-11-21 09:55:19 -0500

    Finished Timer

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+// Circle animation for the SVG
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+// variable to hold what the current offset is for the timer animation on the SVG
+let duration;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("Timer Started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("Ticking");
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
   },
   onComplete() {
     console.log("Timer is done");
